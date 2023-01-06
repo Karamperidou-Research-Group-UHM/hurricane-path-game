62d789b8fa916f10f452bad8cfb32eebb9366b8f
charlie-robison <charlierobison480@gmail.com>
2023-01-06 11:44:57 -1000

    Created the SeasonLabel class which provides methods and properties for a season label.

---
+++ app/gameobjects/seasonLabel.js
@@ -0,0 +1,26 @@
+/** Provides methods and properties for a season label. */
+export default class SeasonLabel {
+  /** Creates a new Season Label. */
+  constructor(gameArea) {
+    this.gameArea = gameArea;
+    this.seasons = ['Spring', 'Summer', 'Fall', 'Winter'];
+    this.currentIndex = 0;
+  }
+
+  /** Changes the season the label will indicate. */
+  changeSeason() {
+    this.currentIndex++;
+
+    // Cycles back to the first index of the seasons array if the current index is greater than the length of the array.
+    if (this.currentIndex >= this.seasons.length) {
+      this.currentIndex = 0;
+    }
+  }
+
+  /** Updates the season label. */
+  update() {
+    const ctx = this.gameArea.context;
+    ctx.font = '48px serif';
+    ctx.fillText(this.seasons[this.currentIndex], 700, 50);
+  }
+}
