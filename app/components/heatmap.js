ad449c0b49d4450135ea801a315f21d484cb6981
charlie-robison <charlierobison480@gmail.com>
2023-01-17 10:53:24 -1000

    Created the HeatMap class which adds heat points to the canvas given the coordinates and provides and update method which updates all heat points.

---
+++ app/components/heatmap.js
@@ -0,0 +1,27 @@
+import HeatPoint from '../gameobjects/heatPoint.js';
+
+/** Creates the properties and methods for a HeatMap object. */
+export default class HeatMap {
+  /** Creates a new HeatMap object. */
+  constructor(coordinates, gameArea) {
+    this.coordinates = coordinates;
+    this.gameArea = gameArea;
+    this.heatPoints = [];
+  }
+
+  /** Adds a heat point to the canvas given the coordinates given. */
+  addHeatPoints() {
+    // Loops through each coordinate, adds the heat point to the canvas and the heatPoints array. */
+    for (let i = 0; i < this.coordinates; i++) {
+      const heatPoint = new HeatPoint(this.coordinates[i].x, this.coordinates[i].y, this.coordinates[i].temp, this.gameArea);
+      this.heatPoints.push(heatPoint);
+    }
+  }
+
+  /** Updates each heat point object in the heat points array. */
+  updateHeatPoints() {
+    for(let i = 0; i < this.heatPoints; i++) {
+      this.heatPoints[i].update();
+    }
+  }
+}
