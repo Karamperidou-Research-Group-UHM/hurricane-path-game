8c23e490a3cafdc0083f26280181b410b7613bc0
charlie-robison <charlierobison480@gmail.com>
2023-01-17 17:27:53 -1000

    Created the updateX and updateY methods in the Hurricane class which updates the Hurricane's position variables.

--- app/gameobjects/hurricane.js
+++ app/gameobjects/hurricane.js
@@ -50,6 +50,20 @@ export default class Hurricane extends GameObject {
     this.y += this.speed * Math.cos(direction);
   }
 
+  /** Updates the hurricane's x position by an amount. */
+  updateX(amount) {
+    if (this.x - this.width >= 0 && this.x + this.width <= 825) {
+      this.x += amount;
+    }
+  }
+
+  /** Updates the hurricane's y position by an amount. */
+  updateY(amount) {
+    if (this.y - this.height >= 0 && this.y + this.height <= 526) {
+      this.y += amount;
+    }
+  }
+
   /** Updates the Hurricane object. */
   update() {
     // this.rotate();
