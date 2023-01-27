d752e7e1fc25be62d7aaeab2d41a6940a0fffc04
charlie-robison <charlierobison480@gmail.com>
2023-01-27 10:54:14 -1000

    Readjusted wind test data to be a little bit more realistic, still not exact and needs real data!

--- app/components/testData.js
+++ app/components/testData.js
@@ -13,7 +13,11 @@ export default class TestData {
   windTestData(windData) {
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 7; j++) {
-        windData.push({x: (100 * i) + 30, y: (100 * j) + 50, windDir: Math.floor(Math.random() * (0 - 360))});
+        if (j < 4) {
+          windData.push({x: (80 * i) + 10, y: (70 * j) + 20, windDir: 140 + Math.pow(2, j)});
+        } else {
+          windData.push({x: (80 * i) + 50, y: (70 * j) + 20, windDir: -10 - (0.5 * Math.pow(2, j))});
+        }
       }
     }
   }
