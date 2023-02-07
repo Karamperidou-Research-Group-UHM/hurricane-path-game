1a59579516a5d2e17c24dff0610a08c6d1a03f43
charlie-robison <charlierobison480@gmail.com>
2023-02-07 10:36:27 -1000

    Removed the HurricaneCollisonCheck class since it was no longer used and the updateY method in Hurricane class.

--- app/gameobjects/hurricane.js
+++ app/gameobjects/hurricane.js
@@ -42,13 +42,6 @@ export default class Hurricane extends GameObject {
     }
   }
 
-  /** Updates the hurricane's y position by an amount.*/
-  updateY(amount) {
-    if (this.y - this.height >= 0 && this.y + this.height <= 526) {
-      this.y += amount;
-    }
-  }
-
   /** Updates the Hurricane object. */
   update() {
     let sst_xPosition = 0;
