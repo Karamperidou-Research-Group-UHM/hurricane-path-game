18dfc41536338e40e4d4813a77c45af9d2a12710
charlie-robison <charlierobison480@gmail.com>
2023-03-23 16:55:19 -1000

    Created a skeleton of the  get() method in views.py in sstdata app.

--- api/sstdata/views.py
+++ api/sstdata/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 # Create your views here.
+class SSTView(APIView):
+    def get(self, request, season):
+        # Checks if the season entered is valid.
+        if (season.lower() != 'spring' or season.lower() != 'summer' or season.lower() != 'fall' or season.lower() != 'winter'):
+            return Response({'status': 'error'})
+        else:
+            return Response({'season entered': season})
