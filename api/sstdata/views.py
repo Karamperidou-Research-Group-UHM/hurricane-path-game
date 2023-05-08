9b54a77c838c2288a7e97077e3437f72bf07ba90
charlie-robison <charlierobison480@gmail.com>
2023-05-08 09:36:19 -1000

    Renamed getData to getSSTData for better communication.

--- api/sstdata/views.py
+++ api/sstdata/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .getData import getSSTData
+from .getSSTData import getSSTData
 
 # Create your views here.
 class SSTView(APIView):
